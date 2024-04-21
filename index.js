ba4e86eba16818e7ea85306a5b4ae4d625ed1040
javeria-zohaib <161313643+javeria-zohaib@users.noreply.github.com>
2024-04-21 14:09:35 -0700

    word counter using typescript,nodejs,inquirer

---
+++ index.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+let answer = await inquirer.prompt([{
+        name: "sentence",
+        type: "input",
+        message: chalk.green("Enter your sentence:"),
+    }]);
+let words = answer.sentence.trim().split(" ");
+console.log("=".repeat(60));
+console.log(chalk.bold("- Sentence Words:"));
+console.log(words);
+console.log(chalk.bold(`\n - Word Count: ${chalk.bold.redBright(words.length)}`));
+console.log("=".repeat(60));
